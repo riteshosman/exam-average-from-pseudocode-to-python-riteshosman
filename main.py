67c2ac053060b70cfc908605182935ce3f7143a2
Ritesh Final scores <ritesh.osman@gmail.com>
2025-11-17 20:25:17 -0600

    Pseudocode

--- main.py
+++ main.py
@@ -18,23 +18,7 @@ Submission Date: 11/17/25
 """
 
 
-def calculate_average():
-    # Request the user to enter the first score. This will be done two more times
-    score1 = float(input("Enter first score: "))
-    # Request the user to enter the second score
-    score2 = float(input("Enter second score: "))
-    # Request the user to enter the third score
-    score3 = float(input("Enter third score: "))
-    # Using the formula below we can calculate the average of the three scores
-    average = (score1 + score2 + score3) / 3
-    # Print the first score.
-    print("First score:", score1)
-    # Print the second score.
-    print("Second score:", score2)
-    # Print the third score
-    print("Third score:", score3)\
-    # Now finally print the average score
-    print("The average score is:", average)
+
 # Call your function when your program is ready
 calculate_average()   
   
