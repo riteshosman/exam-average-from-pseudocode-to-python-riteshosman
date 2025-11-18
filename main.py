201fe224b84dd68164f83aba9b47c525db2a07c1
Ritesh Final scores <ritesh.osman@gmail.com>
2025-11-17 20:26:48 -0600

    Code

--- main.py
+++ main.py
@@ -5,7 +5,17 @@ Assignment Title: Files and Exceptions Project
 Submission Date: 11/17/25
 """
 """
-
+1. Start by defining a function naming it calculate_average
+2. Within the confines of the function, request the user to type the first exam score
+3. Subsequently request the user to type the second exam score
+4. Finally prompt the user to enter the third exam score
+5. The three inputs should then all be converted into numbers
+6. Calculate the average by adding the three scores and dividing it by three
+7. The scores should then be printed on it's own line 
+8. The grand average score should be printed on it's very own line
+9. Terminate the function
+10. Call the function at the bottom of the program 
+"""
 
 
 def calculate_average():
